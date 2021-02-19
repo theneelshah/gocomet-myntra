6dc62480b6cffde7a016957cbded2e930a30ed47
theneelshah <theneelshah@gmail.com>
2021-02-19 21:54:44 +0530

    home page and products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,382 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Breadcrumb,
+  Dropdown,
+  Form,
+  FormControl,
+  InputGroup,
+  Nav,
+  Navbar,
+} from "react-bootstrap";
+import "./App.css";
+import heart from "./assets/heart.png";
+import logo from "./assets/myntra.png";
+import search from "./assets/search.png";
+import bag from "./assets/shopping-bag.png";
+import profile from "./assets/user.png";
+import data from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    searchText: "",
+    product: false,
+    productDetails: {},
+  };
+
+  onFocus = () => {
+    console.log("onFocus");
+  };
+
+  onBlur = () => {
+    console.log("onBlur");
+  };
+
+  onSearch = (e) => {
+    console.log("called");
+    this.setState({ searchText: e.target.value }, () => {
+      console.log(this.state);
+    });
+  };
+
+  openProduct = (el) => {
+    // const { name, description, price, finalPrice, discount, image } = el;
+    this.setState({ product: true, productDetails: el });
+  };
+
+  render() {
+    const { searchText, product, productDetails } = this.state;
+
+    return (
+      <div className="App">
+        <Navbar bg="white" variant="light" className="main-nav">
+          <Navbar.Brand href="/">
+            <img src={logo} alt="" className="logo" />
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Men</Nav.Link>
+            <Nav.Link href="#features">Women</Nav.Link>
+            <Nav.Link href="#pricing">Kids</Nav.Link>
+            <Nav.Link href="#pricing">Home & Living</Nav.Link>
+            <Nav.Link href="#pricing">Offers</Nav.Link>
+          </Nav>
+
+          <Nav className="">
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">
+                  <img src={search} alt="" />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Search for products, brands and more"
+                aria-label="search"
+                aria-describedby="basic-addon1"
+                onFocus={this.onFocus}
+                onBlur={this.onBlur}
+                onChange={this.onSearch}
+              />
+            </InputGroup>
+
+            <div className="side-icons">
+              <div>
+                <img src={profile} alt="" />
+                <p>Profile</p>
+              </div>
+              <div>
+                <img src={heart} alt="" />
+                <p>Wishlist</p>
+              </div>
+              <div>
+                <img src={bag} alt="" />
+                <p>Bag</p>
+              </div>
+            </div>
+          </Nav>
+        </Navbar>
+
+        {!product ? (
+          <div className="main-content">
+            <Breadcrumb className="breadcrumb-nav">
+              <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+              <Breadcrumb.Item href="/">Clothing</Breadcrumb.Item>
+              <Breadcrumb.Item active>Shirts for men & women</Breadcrumb.Item>
+            </Breadcrumb>
+
+            <p className="item-heading">
+              Shirts for Men & Women <span>- 90077 items</span>
+            </p>
+
+            <div className="main">
+              <div className="left">
+                <p className="heading">Filters</p>
+
+                <div className="gender">
+                  <Form.Check type="radio" label="Men" name="gender" id={1} />
+                  <Form.Check type="radio" label="Women" name="gender" id={2} />
+                  <Form.Check type="radio" label="Boys" name="gender" id={3} />
+                  <Form.Check type="radio" label="Girls" name="gender" id={4} />
+                </div>
+
+                <div className="categories">
+                  <p className="filter-heading">Categories</p>
+
+                  <Form.Check
+                    type="checkbox"
+                    label="Shirt (89245)"
+                    name="categories"
+                    id={1}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Sleep Shirt (775)"
+                    name="categories"
+                    id={2}
+                  />
+                </div>
+
+                <div className="brand">
+                  <p className="filter-heading">Brand</p>
+
+                  <Form.Check
+                    type="checkbox"
+                    label="Parx"
+                    name="brand"
+                    id={1}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Roadster"
+                    name="brand"
+                    id={2}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Park Avenue"
+                    name="brand"
+                    id={3}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Blackberrys"
+                    name="brand"
+                    id={4}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="United Colors Of Benetton"
+                    name="brand"
+                    id={5}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="ColorPlus"
+                    name="brand"
+                    id={6}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Raymond"
+                    name="brand"
+                    id={7}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Louis Phillippe"
+                    name="brand"
+                    id={8}
+                  />
+                </div>
+
+                <div className="price">
+                  <p className="filter-heading">Categories</p>
+
+                  <Form.Check
+                    type="checkbox"
+                    label="₹249-₹2929"
+                    name="price"
+                    id={1}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="₹2929 - ₹5619"
+                    name="price"
+                    id={2}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="₹5619 - ₹8309"
+                    name="price"
+                    id={3}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="₹8309 - ₹10999"
+                    name="price"
+                    id={4}
+                  />
+                </div>
+              </div>
+              <div className="right">
+                <div className="top">
+                  <div className="dropdown-wrapper">
+                    <Dropdown>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Bundles
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        <Dropdown.Item href="/">Action</Dropdown.Item>
+                        <Dropdown.Item href="/">Another action</Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Dropdown>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Country Of Origin
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        <Dropdown.Item href="/">All Countries</Dropdown.Item>
+                        <Dropdown.Item href="/">India</Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Dropdown>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Size
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        <Dropdown.Item href="/">S</Dropdown.Item>
+                        <Dropdown.Item href="/">M</Dropdown.Item>
+                        <Dropdown.Item href="/">L</Dropdown.Item>
+                        <Dropdown.Item href="/">XL</Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>
+
+                  <div>
+                    <Dropdown>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Sort By: Recommended
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        <Dropdown.Item href="/">What's New</Dropdown.Item>
+                        <Dropdown.Item href="/">Popularity</Dropdown.Item>
+                        <Dropdown.Item href="/">Better Discount</Dropdown.Item>
+                        <Dropdown.Item href="/">
+                          Price: Low to High
+                        </Dropdown.Item>
+                        <Dropdown.Item href="/">
+                          Price: High to Low
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>
+                </div>
+
+                <div className="products">
+                  {data.map((el, index) => {
+                    const {
+                      name,
+                      description,
+                      price,
+                      finalPrice,
+                      discount,
+                      image,
+                    } = el;
+
+                    if (name.toLowerCase().includes(searchText.toLowerCase()))
+                      return (
+                        <div
+                          key={index}
+                          className="product"
+                          onClick={() => {
+                            this.openProduct(el);
+                          }}
+                        >
+                          <div className="image-holder">
+                            <img src={image} alt="" />
+                            <div className="info">
+                              <div className="wishlist">
+                                <button>Wishlist</button>
+                              </div>
+                              <p className="name">{name}</p>
+                              <p className="desc">{description}</p>
+
+                              <div className="product-price">
+                                <span className="final-price">{`₹${finalPrice}`}</span>
+                                <span className="price">{`₹${price}`}</span>
+                                <span className="discount">{discount}</span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    else return <></>;
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="main-content">
+            <Breadcrumb className="breadcrumb-nav">
+              <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+              <Breadcrumb.Item href="/">Clothing</Breadcrumb.Item>
+              <Breadcrumb.Item>Men Clothing</Breadcrumb.Item>
+              <Breadcrumb.Item active>{productDetails.name}</Breadcrumb.Item>
+            </Breadcrumb>
+
+            <div className="product-page">
+              <div className="image-holder">
+                <img src={productDetails.image} alt="" />
+                <img src={productDetails.image} alt="" />
+                <img src={productDetails.image} alt="" />
+                <img src={productDetails.image} alt="" />
+                <img src={productDetails.image} alt="" />
+                <img src={productDetails.image} alt="" />
+                <img src={productDetails.image} alt="" />
+              </div>
+              <div className="product-details">
+                <h1 className="heading">{productDetails.name}</h1>
+                <p class="description">{productDetails.description}</p>
+                <div class="price-container">
+                  <p class="final-price">₹{productDetails.finalPrice}</p>
+                  <p class="price">₹{productDetails.price}</p>
+                  <p class="discount">({productDetails.discount} OFF)</p>
+                </div>
+                <div class="tax">Inclusive of all the taxes</div>
+                <div class="size-section">
+                  <div class="header">
+                    <p>select size</p>
+                  </div>
+                  <div class="buttons">
+                    <div class="button">39</div>
+                    <div class="button">40</div>
+                    <div class="button">42</div>
+                    <div class="button">44</div>
+                    <div class="button">46</div>
+                  </div>
+                </div>
+                <div class="main-buttons">
+                  <div class="cart-btn">
+                    <img src={bag} alt="" />
+                    Add to Bag
+                  </div>
+                  <div class="wishlist-btn">
+                    <img src={heart} alt="" />
+                    Wishlist
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,112 @@
+const data = [
+  {
+    name: "HIGHLANDER",
+    description: "Slim fit Casual Shirt",
+    price: 999,
+    finalPrice: 449,
+    discount: "55%",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/1265388/2019/3/15/c293452c-bd29-4327-a56d-1e847a26eedc1552650275369-Highlander-Maroon-Slim-Fit-Casual-Shirt-1801552650274001-1.jpg",
+  },
+  {
+    name: "Levis",
+    description: "twill weave slim fit shirt",
+    price: 1799,
+    finalPrice: 999,
+    discount: "₹800",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/1376577/2016/9/15/11473928353466-Roadster-Men-Black-Regular-Fit-Checked-Casual-Shirt-4501473928353310-1.jpg",
+  },
+  {
+    name: "Bene Kleed",
+    description: "men slim fit casual shirt",
+    price: 2099,
+    finalPrice: 671,
+    discount: "68%",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/7189947/2018/8/30/b0a17130-00b2-47dd-9acf-75fcdf7333111535614137835-Bene-Kleed-Men-Off-White--Blue-Slim-Fit-Printed-Casual-Shirt-3181535614137565-1.jpg",
+  },
+  {
+    name: "Roadster",
+    description: "printed casual shirt",
+    price: 1499,
+    finalPrice: 674,
+    discount: "55%",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/2414313/2018/3/13/11520926368526-HERENOW-Men-Red--Black-Regular-Fit-Checked-Casual-Shirt-8881520926368447-1.jpg",
+  },
+  {
+    name: "Highlander",
+    description: "men slim fit casual shirt",
+    price: 999,
+    finalPrice: 519,
+    discount: "48%",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/1291760/2017/12/5/11512469309123-Highlander-Dark-Green-Slim-Fit-Casual-Shirt-5071512469308877-1.jpg",
+  },
+  {
+    name: "Allen SOlly",
+    description: "slim fit solid casual shirt",
+    price: 1699,
+    finalPrice: 1529,
+    discount: "10%",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/1/20/d23d936c-b7ee-46d2-9d8c-da6458f511cd1611135522989-1.jpg",
+  },
+  {
+    name: "Levis",
+    description: "slim fit solid casual shirt",
+    price: 2999,
+    finalPrice: 1799,
+    discount: "40%",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2019/7/27/110eefa8-e43b-4c42-85f7-83592dc9c9701564175877424-1.jpg",
+  },
+  {
+    name: "Roadster",
+    description: "reversible casual shirt",
+    price: 1599,
+    finalPrice: 1119,
+    discount: "30%",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/12407916/2020/11/18/22b3baa7-5b5a-4c96-887c-1d71114c8fd11605688730684JacketsPumaMenJacketsPumaMenJacketsPumaMenJacketsPumaMenJack1.jpg",
+  },
+  {
+    name: "Highlander",
+    description: "slim casual shirt",
+    price: 999,
+    finalPrice: 479,
+    discount: "52%",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/2129960/2019/12/4/c5f17385-a6c1-4727-982f-b27a7780bf211575437376659-HIGHLANDER-Men-Navy--Red-Slim-Fit-Checked-Casual-Shirt-14615-1.jpg",
+  },
+  {
+    name: "HERE&NOW",
+    description: "men regular fit casual shirt",
+    price: 1499,
+    finalPrice: 824,
+    discount: "45%",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/2042528/2017/9/28/11506599316866-Mast--Harbour-Men-Red--Blue-Regular-Fit-Checked-Casual-Shirt-7991506599316924-1.jpg",
+  },
+  {
+    name: "HERE&NOW",
+    description: "men regular fit casual shirt",
+    price: 1499,
+    finalPrice: 674,
+    discount: "55%",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/7488103/2019/8/22/7142624c-9184-47db-bf67-5610bd756c761566454100661-Dennis-Lingo-Men-Green-Slim-Fit-Solid-Casual-Shirt-358156645-1.jpg",
+  },
+  {
+    name: "Levis",
+    description: "slim fit  casual shirt",
+    price: 1799,
+    finalPrice: 999,
+    discount: "₹800",
+    image:
+      "https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/13156956/2021/2/11/2b16f4bd-a83a-4650-97a4-9ca19cc385361613020991873-Louis-Philippe-Sport-Men-Shirts-6291613020989994-1.jpg",
+  },
+];
+
+export default data;
